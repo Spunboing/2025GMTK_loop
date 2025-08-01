abfffda75f3fbd15838ddb421f00e09140df9c83
Spunboing <diamondjellybean457@gmail.com>
2025-08-01 14:34:13 -0700

    fixed fireball spawner

---
+++ scenes&scripts/global_data.gd
@@ -0,0 +1,9 @@
+extends Node
+
+var highest_score: int = 0
+var first_play: bool = false
+
+func game_over(recent_score):
+	if recent_score > highest_score:
+		highest_score = recent_score
+			
--- scenes&scripts/obstacle_spawner.gd
+++ scenes&scripts/obstacle_spawner.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
+@onready var timer: Timer = $Timer
 var fireball = preload("res://scenes&scripts/obstacles/fireball.tscn")
 var activate : bool = false
-func _process(delta: float) -> void:
+
+func _ready():
 	await get_tree().create_timer(3).timeout
+	_on_timer_timeout()
+
+func _on_timer_timeout() -> void:
 	activate = true
 	
 	for n in round(randf_range(4, 8)):
@@ -13,3 +18,4 @@ func _process(delta: float) -> void:
 			$"../..".add_child(instance)
 			await get_tree().create_timer(1).timeout
 	activate = false
+	timer.start()
