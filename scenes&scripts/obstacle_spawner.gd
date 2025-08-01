d1a3c070e04a0257eef70563dd1088c35ec15262
U-LAPTOP-6LK8RHLO\filip <filip92027@gmail.com>
2025-08-01 23:17:51 +0200

    can somebody fix the obstacle spawner?

--- scenes&scripts/obstacle_spawner.gd
+++ scenes&scripts/obstacle_spawner.gd
@@ -1,9 +1,15 @@
 extends Node2D
 
 var fireball = preload("res://scenes&scripts/obstacles/fireball.tscn")
-
+var activate : bool = false
 func _process(delta: float) -> void:
-	await get_tree().create_timer(3000).timeout
+	await get_tree().create_timer(3).timeout
+	activate = true
 	
 	for n in round(randf_range(4, 8)):
-		add_child(fireball.instantiate())
+		if activate:
+			var instance = fireball.instantiate()
+			instance.global_position = global_position
+			$"../..".add_child(instance)
+			await get_tree().create_timer(1).timeout
+	activate = false
