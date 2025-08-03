82105452be1d81e939dffe7338a5f9bfaae8b69e
Spunboing <diamondjellybean457@gmail.com>
2025-08-03 01:09:14 -0700

    added global music controller frfr on god no cap

--- scenes&scripts/hoop.gd
+++ scenes&scripts/hoop.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
 var used: bool = false
+@onready var hoop_sfx: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if !used:
 		if body.is_in_group("player"):
+			hoop_sfx.play()
 			body.add_score_mult(1)
 			used = true
